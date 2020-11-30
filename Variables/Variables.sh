5e584a51668a5a284e3e69afd1d20302f37363e4
Rafael-99-spec <60171771+Rafael-99-spec@users.noreply.github.com>
2020-11-30 15:20:13 +0200

    Update and rename Variables to Variables.sh

---
+++ Variables/Variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# A backslash "\" is used to escape special character meaning
+#___________________________________________________________________
+
+PRICE_PER_APPLE=5
+echo "The price of an Apple today is: \$HK $PRICE_PER_APPLE"
+
+#___________________________________________________________________
+
