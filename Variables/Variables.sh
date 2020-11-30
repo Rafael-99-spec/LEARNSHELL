95cd07ad6aa8520544248e1390bbf84f9b0258a7
Rafael-99-spec <60171771+Rafael-99-spec@users.noreply.github.com>
2020-11-30 15:34:25 +0200

    Update Variables.sh

--- Variables/Variables.sh
+++ Variables/Variables.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 
-#_1) A backslash "\" is used to escape special character meaning
+
+#_1) Shell variables are created once they are assigned a value. 
+# A variable can contain a number, a character or a string of characters. 
+# Variable name is case sensitive and can consist of a combination of letters 
+# and the underscore "_". Value assignment is done using the "=" sign. Note 
+# that no space permitted on either side of = sign when initializing variables.
+
+PRICE_PER_APPLE=5
+MyFirstLetters=ABC
+greeting='Hello        world!'
+
+#___________________________________________________________________
+
+
+#_2) A backslash "\" is used to escape special character meaning
 
 PRICE_PER_APPLE=5
 echo "The price of an Apple today is: \$HK $PRICE_PER_APPLE"
@@ -8,7 +22,7 @@ echo "The price of an Apple today is: \$HK $PRICE_PER_APPLE"
 #___________________________________________________________________
 
 
-#_2) Encapsulating the variable name with ${} is used to avoid ambiguity
+#_3) Encapsulating the variable name with ${} is used to avoid ambiguity
 
 MyFirstLetters=ABC
 echo "The first 10 letters in the alphabet are: ${MyFirstLetters}DEFGHIJ"
@@ -16,7 +30,7 @@ echo "The first 10 letters in the alphabet are: ${MyFirstLetters}DEFGHIJ"
 #___________________________________________________________________
 
 
-#_3) Encapsulating the variable name with "" will preserve any white space values
+#_4) Encapsulating the variable name with "" will preserve any white space values
 
 greeting='Hello        world!'
 echo $greeting" now with spaces: $greeting"
@@ -24,12 +38,13 @@ echo $greeting" now with spaces: $greeting"
 #___________________________________________________________________
 
 
-#_4) Variables can be assigned with the value of a command output. 
+#_5) Variables can be assigned with the value of a command output. 
 # This is referred to as substitution. Substitution can be done by 
 # encapsulating the command with `` (known as back-ticks) or with $()
 
 FILELIST=`ls`
 FileWithTimeStamp=/tmp/my-dir/file_$(/bin/date +%Y-%m-%d).txt
 
+# Note that when the script runs, it will run the command inside the $() parenthesis and capture its output.
 #___________________________________________________________________
 
