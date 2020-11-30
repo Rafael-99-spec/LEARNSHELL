03efbd53577bcffb737323d84643fde87840b6a9
Rafael-99-spec <60171771+Rafael-99-spec@users.noreply.github.com>
2020-11-30 15:22:22 +0200

    Update Variables.sh

--- Variables/Variables.sh
+++ Variables/Variables.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
-# A backslash "\" is used to escape special character meaning
-#___________________________________________________________________
+#_1) A backslash "\" is used to escape special character meaning
 
 PRICE_PER_APPLE=5
 echo "The price of an Apple today is: \$HK $PRICE_PER_APPLE"
 
 #___________________________________________________________________
 
+
+#_2) Encapsulating the variable name with ${} is used to avoid ambiguity
+
+MyFirstLetters=ABC
+echo "The first 10 letters in the alphabet are: ${MyFirstLetters}DEFGHIJ"
+
+#___________________________________________________________________
